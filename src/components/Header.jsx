21983d2f0b526fc66b958d8d5307764c54d7a80d
Zachary F Jorgensen <jorgensenzachary00@gmail.com>
2024-11-12 13:59:45 -0800

    added dropdown button for quick page access

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
 export default function Header() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Jorgensen Dog Walking
           </a>
-          <button class="btn btn-outline-success" type="submit">
-            Search
-          </button>
+          <div>
+            <button
+              className="btn"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i className="bi bi-list"></i>
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
